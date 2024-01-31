c38b93c60135763c4aa9ee4b9acaf71570503067
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-01-31 03:53:28 -0600

    Se crea funcion para a;adir datos a la tabla

--- assets/script/contactoScript.js
+++ assets/script/contactoScript.js
@@ -5,4 +5,18 @@ function captura(){
     var com = document.getElementById("comentario").value;
     console.log(name + " " + email)
 
+     insertarFila(name,email,tel);
+
+}
+
+function insertarFila(name,email,tel){
+    let tblDatos = document.getElementById('tblDatos').insertRow(1);
+    let col1 = tblDatos.insertCell(0);
+    let col2 = tblDatos.insertCell(1);
+    let col3 = tblDatos.insertCell(2);
+
+
+    col1.innerHTML = name;
+    col2.innerHTML = email;
+    col3.innerHTML = tel;
 }

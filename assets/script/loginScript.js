37aa1b1d8b391dd70313ee7c2b214163e4c7385d
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-01-31 02:11:02 -0600

    Se crean nuevos usuarios

--- assets/script/loginScript.js
+++ assets/script/loginScript.js
@@ -5,7 +5,8 @@ function login(){
 
     pass = document.getElementById("password").value;
 
-    if(user == "hgomez-2022207" && pass == "12345" || user == "bgomez-2022208" && pass == "12345"){
+    if(user == "hgomez-2022207" && pass == "12345" || user == "bgomez-2022208" && pass == "12345" || user == "ggarcia-2022205" && pass == "12345"
+    || user == "becheveria" && pass == "12345"){
         window.location = "./assets/contactos.html"
     }
 } 

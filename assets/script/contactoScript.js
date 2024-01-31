b9225184cd77a4ca7bf68da24c0eff8e1c9ea34f
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-01-31 03:27:18 -0600

    Se crea el funcionamiento del boton agregar

---
+++ assets/script/contactoScript.js
@@ -0,0 +1,8 @@
+function captura(){
+    var name = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var tel = document.getElementById("telefono").value;
+    var com = document.getElementById("comentario").value;
+    console.log(name + " " + email)
+
+}

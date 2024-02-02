6fa870385530d86ee5286ad917c73e3c5b196cf9
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-02-02 01:14:34 -0600

    Se a;aden comentarios

--- assets/script/contactoScript.js
+++ assets/script/contactoScript.js
@@ -5,18 +5,24 @@ function captura(){
     var com = document.getElementById("comentario").value;
     console.log(name + " " + email)
 
-     insertarFila(name,email,tel);
+     insertarFila(name,email,tel,com);
 
 }
 
-function insertarFila(name,email,tel){
+function insertarFila(name,email,tel,com){
     let tblDatos = document.getElementById('tblDatos').insertRow(1);
     let col1 = tblDatos.insertCell(0);
     let col2 = tblDatos.insertCell(1);
     let col3 = tblDatos.insertCell(2);
+    let col4 = tblDatos.insertCell(3);
 
 
     col1.innerHTML = name;
     col2.innerHTML = email;
     col3.innerHTML = tel;
+    col4.innerHTML = com;
+}
+
+function abrirUsuario() {
+    window.open("usuario.html", "Usuario", "width=600,height=400");
 }

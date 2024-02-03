2a01213881a891ce4453016d09e06e9076648119
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-02-02 21:32:21 -0600

    Se crea dise;o responsive para todos los archivos

--- assets/script/tareas.js
+++ assets/script/tareas.js
@@ -9,13 +9,41 @@ function captura(){
 function insertarFila(tarea){
     let tblTareas = document.getElementById('tblTareas').insertRow(1);
     let col1 = tblTareas.insertCell(0);
-    let col2 = tblTareas.insertCell(1);
+    let col2 = tblTareas.insertCell(1)
+ 
 
 
     col1.innerHTML = tarea;
-    col2.innerHTML = email;
+    eliminarbtn = document.createElement("button");
+    eliminarbtn.textContent = "Eliminar";
+    col2.appendChild(eliminarbtn);
+
+    eliminarbtn.addEventListener("click", (event) =>{
+        
+        event.target.parentNode.parentNode.remove();
+    });
 }
 
-function abrirUsuario() {
-    window.open("usuario.html", "Usuario", "width=600,height=400");
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll(".eliminar-btn").forEach((button) => {
+        button.addEventListener("click", (event) => {
+            event.target.parentNode.parentNode.remove();
+        });
+    });
+});
+
+
+eliminarbtn.addEventListener("click", (event) =>{
+        
+    event.target.parentNode.parentNode.remove();
+});
+
+function eliminar() {
+
+    target.parentNode.parentNode.remove();
+
+   /* eliminarbtn.addEventListener("click", (event) =>{
+        
+        event.target.parentNode.parentNode.remove();
+    }); */
 }
